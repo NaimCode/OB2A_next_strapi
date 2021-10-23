e97edc83045693eeadb756681182a28ef5c71fce
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2021-10-23 18:11:29 +0100

    client api

--- pages/panier.js
+++ pages/panier.js
@@ -1,6 +1,6 @@
 const Panier = () => {
   return (
-    <div className="flex flex-col md:flex-row py-28 h-screen w-screen bg-red-500">
+    <div className="flex flex-col md:flex-row py-28 h-screen w-screen bg-primary-700">
       <div className="flex-grow h-96 bg-blue-300 w-full"></div>
     </div>
   );
